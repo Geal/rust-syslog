b670a0e9e677239d5843eef0bb1da95b7f3163a0
Geoffroy Couprie <geo.couprie@gmail.com>
2015-07-02 14:01:06 +0200

    Update to latest Rust

--- examples/write.rs
+++ examples/write.rs
@@ -3,10 +3,10 @@ extern crate syslog;
 use syslog::{Facility,Severity};
 
 fn main() {
-  match syslog::init(String::from_str("add"), Facility::LOG_USER, String::from_str("example")) {
-    Err(e)         => println!("impossible to connect to syslog: {}", e.desc),
+  match syslog::init(String::from("add"), Facility::LOG_USER, String::from("example")) {
+    Err(e)         => println!("impossible to connect to syslog: {:?}", e),
     Ok(mut writer) => {
-      let r = writer.send(Severity::LOG_ALERT, String::from_str("hello world"));
+      let r = writer.send(Severity::LOG_ALERT, String::from("hello world"));
       if r.is_err() {
         println!("error sending the log {}", r.err().expect("got error"));
       }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
 #![crate_type = "lib"]
 
-extern crate alloc;
-#[cfg(test)] extern crate test;
+extern crate unix_socket;
+extern crate rand;
 
-use std::old_io;
 use std::result::Result;
-use std::ffi::CString;
-use std::old_io::fs::PathExtensions;
-//use std::path::posix::Path;
-use std::rand::{thread_rng, Rng};
+use std::io;
+use std::path::Path;
+use rand::{thread_rng, Rng};
 
-mod unixdatagram;
+use unix_socket::UnixDatagram;
 
 pub type Priority = u8;
 
@@ -60,33 +58,36 @@ pub struct Writer {
   raddr:    String,
   client:   String,
   server:   String,
-  s:        Box<unixdatagram::UnixDatagram>
+  s:        Box<UnixDatagram>
 }
 
-pub fn init(address: String, facility: Facility, tag: String) -> Result<Box<Writer>, old_io::IoError> {
+pub fn init(address: String, facility: Facility, tag: String) -> Result<Box<Writer>, io::Error> {
   let mut path = "/dev/log".to_string();
-  if ! Path::new(path.clone()).stat().is_ok() {
+  if ! std::fs::metadata(Path::new(&path)).is_ok() {
     path = "/var/run/syslog".to_string();
-    if ! Path::new(path.clone()).stat().is_ok() {
-      return Err(old_io::IoError {
-        kind: old_io::PathDoesntExist,
-        desc: "could not find /dev/log nor /var/run/syslog",
-        detail: None
-      });
+    if ! std::fs::metadata(Path::new(&path)).is_ok() {
+      return Err(
+        io::Error::new(
+          io::ErrorKind::NotFound,
+          "could not find /dev/log nor /var/run/syslog"
+        )
+      );
     }
   }
   match tempfile() {
     None => {
       println!("could not generate a tempfile");
-      Err(old_io::IoError {
-        kind: old_io::PathAlreadyExists,
-        desc: "could not generate a temporary file",
-        detail: None
-      })
+      Err(
+        io::Error::new(
+          io::ErrorKind::AlreadyExists,
+          "could not generate a temporary file"
+        )
+      )
     },
     Some(p) => {
       //println!("temp file: {}", p);
-      unixdatagram::UnixDatagram::bind(&CString::from_slice(p.as_bytes())) .map( |s| {
+      //unixdatagram::UnixDatagram::bind(&CString::from_slice(p.as_bytes())) .map( |s| {
+      UnixDatagram::bind(&p) .map( |s| {
         Box::new(Writer {
           facility: facility.clone(),
           tag:      tag.clone(),
@@ -122,47 +123,47 @@ impl Writer {
     return facility as u8 | severity as u8
   }
 
-  pub fn send(&mut self, severity: Severity, message: String) -> Result<(), old_io::IoError> {
+  pub fn send(&mut self, severity: Severity, message: String) -> Result<usize, io::Error> {
     let formatted = self.format(severity, message).into_bytes();
-    self.s.sendto(formatted.as_slice(), &CString::from_slice(self.server.as_bytes()))
+    self.s.send_to(&formatted[..], Path::new(&self.server))
   }
 
-  pub fn emerg(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn emerg(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_EMERG, message)
   }
 
-  pub fn alert(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn alert(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_ALERT, message)
   }
 
-  pub fn crit(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn crit(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_CRIT, message)
   }
 
-  pub fn err(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn err(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_ERR, message)
   }
 
-  pub fn warning(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn warning(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_WARNING, message)
   }
 
-  pub fn notice(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn notice(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_NOTICE, message)
   }
 
-  pub fn info(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn info(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_INFO, message)
   }
 
-  pub fn debug(&mut self, message: String) -> Result<(), old_io::IoError> {
+  pub fn debug(&mut self, message: String) -> Result<usize, io::Error> {
     self.send(Severity::LOG_DEBUG, message)
   }
 }
 
 impl Drop for Writer {
   fn drop(&mut self) {
-    let r = old_io::fs::unlink(&Path::new(self.client.clone()));
+    let r = std::fs::remove_file(&Path::new(&self.client.clone()));
     if r.is_err() {
       println!("could not delete the client socket: {}", self.client);
     }
@@ -172,11 +173,12 @@ impl Drop for Writer {
 fn tempfile() -> Option<String> {
   let tmpdir = Path::new("/tmp");
   let mut r = thread_rng();
-  for _ in range(0, 1000) {
+  for _ in 0..1000 {
     let filename: String = r.gen_ascii_chars().take(16).collect();
     let p = tmpdir.join(filename);
-    if ! p.stat().is_ok() {
-      return p.as_str().map(|s| s.to_string());
+    if ! std::fs::metadata(&p).is_ok() {
+      //return p.as_str().map(|s| s.to_string());
+      return p.to_str().map(|s| String::from(s));
     }
   }
   None
