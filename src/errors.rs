b6776638d6f32d5f4a7321c06678b2cc3273e467
Geoffroy Couprie <contact@geoffroycouprie.com>
2024-07-26 16:12:51 +0200

    remove error-chain

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,34 @@
-error_chain! {
-    errors { Initialization UnsupportedPlatform Format Write }
+#[derive(Debug)]
+pub enum Error {
+    Initialization(Box<dyn std::error::Error + Send + Sync>),
+    Write(::std::io::Error),
+    Io(::std::io::Error),
+}
 
-    foreign_links {
-        Io(::std::io::Error) #[doc = "Link to a `std::error::Error` type."];
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl std::error::Error for Error {
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        match *self {
+            Error::Initialization(ref err) => Some(&**err),
+            Error::Write(_) => None,
+            Error::Io(ref err) => Some(err),
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::Initialization(ref err) => write!(f, "Initialization error: {}", err),
+            Error::Write(ref err) => write!(f, "Write error: {}", err),
+            Error::Io(ref err) => write!(f, "Io error: {}", err),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Error {
+        Error::Io(err)
     }
 }
